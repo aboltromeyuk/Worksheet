ecc9ede3543676d646ebf8bf72e99c5c3b51f3a1
Artem Boltromeyuk <artem.boltromeyuk@intravision.ru>
2018-05-06 17:57:58 +0300

    fix

--- Worksheet.ViewModels/FullViewModel.cs
+++ Worksheet.ViewModels/FullViewModel.cs
@@ -12,7 +12,7 @@ namespace Worksheet.ViewModels
         public StandingViewModel Standing { get; set; }
 
         [Required]
-        [DisplayName("Вы ознакомились с правилами \"Кубка прогнозов\"? Отвечая да, вы принимаете условия проекта.")]
+        [DisplayName("Вы ознакомились с правилами \"Кубка прогнозов\"? Отвечая да, вы принимаете условия проекта и даёте своё согласие на обработку ваших персональных данных.")]
         public bool Agreement { get; set; }        
     }
 }
